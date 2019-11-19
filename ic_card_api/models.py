7f8d3c5878aeea337fb0abdc1d8d7e934899f41c
yuya <sasaki.y@ruri.waseda.jp>
2019-11-19 09:10:40 +0900

    set uuid of device to editable.

--- ic_card_api/models.py
+++ ic_card_api/models.py
@@ -19,7 +19,7 @@ class Device(models.Model):
     from_office_brunch = models.ForeignKey(to=OfficeBrunch, on_delete=models.CASCADE, related_name="from_office_brunch")
     to_office_brunch = models.ForeignKey(to=OfficeBrunch, on_delete=models.CASCADE, related_name="to_office_brunch")
     name = models.CharField(max_length=100)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
 
     def __str__(self):
         return self.name
