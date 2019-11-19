a08f877273937e8ba8a6f3c14de8745ab5b20005
yuya <sasaki.y@ruri.waseda.jp>
2019-11-19 09:03:37 +0900

    added __str__ method to models.

--- ic_card_api/models.py
+++ ic_card_api/models.py
@@ -11,12 +11,18 @@ import uuid
 class OfficeBrunch(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Device(models.Model):
     from_office_brunch = models.ForeignKey(to=OfficeBrunch, on_delete=models.CASCADE, related_name="from_office_brunch")
     to_office_brunch = models.ForeignKey(to=OfficeBrunch, on_delete=models.CASCADE, related_name="to_office_brunch")
     name = models.CharField(max_length=100)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Ride(models.Model):
@@ -24,7 +30,5 @@ class Ride(models.Model):
     member_id = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_created=True)
 
-
-
-
-
+    def __str__(self):
+        return self.member_id + " at " + str(self.created_at)
